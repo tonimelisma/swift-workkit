4dd3b8b5ac67121be2d9fe7d6f74d2d7a863fbeb
Toni Melisma <59022391+tonimelisma@users.noreply.github.com>
2026-07-15 12:08:38 -0700

    Initial Commit

---
+++ Work Agent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Work Agent
+//
+//  Created by Toni Melisma on 7/15/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Work Agent/Work_AgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Work_AgentApp.swift
+//  Work Agent
+//
+//  Created by Toni Melisma on 7/15/26.
+//
+
+import SwiftUI
+
+@main
+struct Work_AgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
